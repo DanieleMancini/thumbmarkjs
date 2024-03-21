3693759d1f92c46ca9f2fad2fd4b727c799bc0ec
DanieleMancini <daniel.mancini91@gmail.com>
2024-03-21 22:47:08 +0100

    Revert "need dist for staging"
    This reverts commit d132a49305b1b18d3d815e43e420b49d42681988.

--- dist/types/fingerprint/options.d.ts
+++
@@ -1,8 +0,0 @@
-export interface optionsInterface {
-    exclude?: string[];
-    include?: string[];
-    webgl_runs?: number;
-    canvas_runs?: number;
-}
-export declare let options: optionsInterface;
-export declare function setOption<K extends keyof optionsInterface>(key: K, value: optionsInterface[K]): void;
